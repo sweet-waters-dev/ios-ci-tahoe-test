9e66d44d7213dee34d9b3bf1b395fd8d26813fa2
sweet-waters-dev <dontjoshme@icloud.com>
2025-10-24 10:08:49 -0500

    Initial commit: minimal SwiftUI app for CI test

---
+++ ios-ci-tahoe-test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios-ci-tahoe-test
+//
+//  Created by Joshua Mullenberg on 10/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios-ci-tahoe-test/ios_ci_tahoe_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_ci_tahoe_testApp.swift
+//  ios-ci-tahoe-test
+//
+//  Created by Joshua Mullenberg on 10/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ios_ci_tahoe_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
